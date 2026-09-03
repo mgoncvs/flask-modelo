b4e48ed36592b3d025e584a63b3955515c9739b3
dudão <eduarda.megoncalves@gmail.com>
2026-09-03 13:32:31 +0000

    309

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect , session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave-super-secreta'
 
 @app.route("/")
 def login():
@@ -8,7 +9,9 @@ def login():
 
 @app.route("/painel")
 def painel():
-    return render_template("painel.html")
+    if 'usuario_nome' in session:
+        return render_template("painel.html",usuario_nome=session['usuario_nome'],usuario_cpf=session['usuario_cpf'])
+    return redirect('/')
 
 @app.route("/verificar", methods=['POST'])
 def verificar():
@@ -17,6 +20,22 @@ def verificar():
 
     print('tentando login com: ', cpf,'/senha: ',senha)
     
+    if cpf == "07771232159" and senha == "4295":
+        session['usuario_nome'] = "maria"
+        session['usuario_cpf'] = cpf
+        return redirect('/painel')
+    
+    if cpf=="044223345608" and senha == "123454":
+        session['usuario_nome'] = "taylor"
+        session['usuario_cpf'] = cpf
+        return redirect('/painel')
+
+    return redirect('/')
+
+@app.route('/Sair')
+def Sair():
+    session.pop('usuario_nome',None)
+    session.pop('usuario_cpf',None)
     return redirect('/')
 
 if __name__ == "__main__":
